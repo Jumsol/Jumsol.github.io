5c948c603bde23ca2507d22d1099f7fe4faa978f
Jum <jumrylsolivio101@gmail.com>
2024-04-21 13:43:31 +0800

    Update Questions

--- Clerical_Ability/spelling.js
+++ Clerical_Ability/spelling.js
@@ -13,32 +13,30 @@ let availableQuesions = [];
 
 let questions = [];
 
-// Load questions from the JSON file
 fetch('/Clerical_Ability/spellingData.json')
-  .then((res) => res.json())
-  .then((loadedQuestions) => {
-    questions = loadedQuestions.map((loadedQuestion) => {
-      const formattedQuestion = {
-        question: loadedQuestion.question,
-      };
+    .then((res) => res.json())
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
 
-      const answerChoices = Object.values(loadedQuestion.choices);
-      formattedQuestion.answer = loadedQuestion.answer;
+            const answerChoices = Object.values(loadedQuestion.choices);
+            formattedQuestion.answer = loadedQuestion.answer;
 
-      answerChoices.forEach((choice, index) => {
-        formattedQuestion['choice' + (index + 1)] = choice;
-      });
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
 
-      return formattedQuestion;
+            return formattedQuestion;
+        });
+
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
     });
 
-    startGame();
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-//CONSTANTS
 const CORRECT_BONUS = 1;
 const MAX_QUESTIONS = 50;
 
@@ -56,22 +54,19 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        //go to the end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerHTML = currentQuestion.question;
 
-        // Remove highlighting from previous question's answer choices
-        choices.forEach((choice) => {
-            choice.parentElement.classList.remove('correct', 'incorrect');
-        });
+    choices.forEach((choice) => {
+        choice.parentElement.classList.remove('correct', 'incorrect');
+    });
 
     choices.forEach((choice) => {
         const number = choice.dataset['number'];
@@ -93,14 +88,11 @@ choices.forEach((choice) => {
         const classToApply =
             selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-            if (classToApply === 'correct') {
-                incrementScore(CORRECT_BONUS);
-                 // Play the correct sound
-                 correctSound.play();
-            } else {
-                 // Play the wrong sound
-                 wrongSound.play();
-            // Highlight the correct answer if the selected answer is wrong
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+            correctSound.play();
+        } else {
+            wrongSound.play();
             const correctChoice = choices.find(
                 (choice) => choice.dataset['number'] == currentQuestion.answer
             );
@@ -109,7 +101,6 @@ choices.forEach((choice) => {
 
         selectedChoice.parentElement.classList.add(classToApply);
 
-        // Show the "Next Question" button after a short delay
         setTimeout(() => {
             document.getElementById('nextBtn').classList.remove('hidden');
         }, 1000);
@@ -128,11 +119,8 @@ document.getElementById('nextBtn').addEventListener('click', () => {
 
 const quitGame = () => {
     if (confirm("Are you sure you want to quit the game?")) {
-        // If user confirms, finish the game
         localStorage.setItem('mostRecentScore', score);
         window.location.assign('/end.html');
     }
 };
-
-// Add a click event listener to the quit button
 document.getElementById('quitBtn').addEventListener('click', quitGame);
